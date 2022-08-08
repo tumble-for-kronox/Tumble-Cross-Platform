aa0b8d53d010ceb7202fe993c959c28f6acf352f
adisve <adis.veletanlic@gmail.com>
2022-08-09 00:22:52 +0200

    Add init script for devs

---
+++ developer_init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+# Script can be run whenever repo is cloned from GitHub or
+# whenever you want to clean up the workspace and re-generate stale files
+
+dir="log"
+if [ ! -d $dir ]; then
+  mkdir -p $dir;
+fi
+echo "Cleaning flutter workspace.."
+flutter clean
+echo "Acquiring necessary flutter pub packages from pubspec.yaml file.."
+flutter pub get > $dir/log.txt
+echo "Generating freezed models under sub-directories in lib/models/.."
+flutter pub run build_runner build --delete-conflicting-outputs > $dir/log.txt
+echo "Verbose output directed to $dir/log.txt"
+exit 1
