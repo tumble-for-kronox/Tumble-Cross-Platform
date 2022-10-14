be87c8f5a2329b34ee63073754decaeaf5ad8919
Adis Veletanlic <adis.veletanlic@gmail.com>
2022-10-14 09:19:40 +0200

    Add notification title fix and correct description to errors in search page

--- lib/core/extensions/response_extensions/schedule_and_programme_parser.dart
+++ lib/core/extensions/response_extensions/schedule_and_programme_parser.dart
@@ -9,16 +9,22 @@ import 'package:tumble/core/ui/data/string_constants.dart';
 extension ScheduleOrProgrammeResponseParsing on Response {
   ScheduleOrProgrammeResponse parseSchedule() {
     if (statusCode == 200) {
-      return ScheduleOrProgrammeResponse.fetched(scheduleModelFromJson(jsonEncode(data)));
+      return ScheduleOrProgrammeResponse.fetched(
+          scheduleModelFromJson(jsonEncode(data)));
     }
-    return ScheduleOrProgrammeResponse.error(RuntimeErrorType.scheduleFetchError(), S.popUps.timeOutDecsription());
+    return ScheduleOrProgrammeResponse.error(
+        RuntimeErrorType.scheduleFetchError(),
+        S.popUps.scheduleHelpFirstLine());
   }
 
   ScheduleOrProgrammeResponse parsePrograms() {
     if (statusCode == 200) {
-      return ScheduleOrProgrammeResponse.fetched(programModelFromJson(jsonEncode(data)));
+      return ScheduleOrProgrammeResponse.fetched(
+          programModelFromJson(jsonEncode(data)));
     }
-    return ScheduleOrProgrammeResponse.error(RuntimeErrorType.programFetchError(), S.popUps.timeOutDecsription());
+    return ScheduleOrProgrammeResponse.error(
+        RuntimeErrorType.programFetchError(),
+        S.popUps.missingProgramRequestDescription());
   }
 }
 
