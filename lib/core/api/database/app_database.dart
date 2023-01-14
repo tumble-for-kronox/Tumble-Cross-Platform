befcb3e9adb8d9222fe77ed8a02500274538dab6
adisve <adis.veletanlic@gmail.com>
2023-01-14 16:56:29 +0100

    Remove unnecessary interfaces and slight code refactoring in certain files

--- lib/core/api/database/app_database.dart
+++ lib/core/api/database/app_database.dart
@@ -1,18 +1,10 @@
 import 'dart:async';
 import 'dart:developer';
-import 'package:flutter/animation.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sembast/sembast.dart';
 import 'package:sembast/sembast_io.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tumble/core/api/database/data/access_stores.dart';
-import 'package:tumble/core/api/database/repository/database_service.dart';
-import 'package:tumble/core/api/dependency_injection/get_it.dart';
-import 'package:tumble/core/api/shared_preferences/shared_preference_service.dart';
-import 'package:tumble/core/models/backend_models/schedule_model.dart';
-import 'package:tumble/core/theme/color_picker.dart';
-import 'package:tumble/core/ui/schedule/utils/day_list_builder.dart';
 
 class AppDatabase {
   // Completer is used for transforming synchronous code into asynchronous code.
@@ -33,7 +25,8 @@ class AppDatabase {
     final database = await databaseFactoryIo.openDatabase(
       dbPath,
       version: 2,
-      onVersionChanged: (db, oldVersion, newVersion) async => _updateDatabase(db, oldVersion, newVersion),
+      onVersionChanged: (db, oldVersion, newVersion) async =>
+          _updateDatabase(db, oldVersion, newVersion),
     );
     _dbOpenCompleter!.complete(database);
   }
@@ -41,10 +34,8 @@ class AppDatabase {
   Future<void> _updateDatabase(db, oldVersion, newVersion) async {
     if (oldVersion < 2) {
       await intMapStoreFactory.store(AccessStores.schedule_store).delete(db);
-      await intMapStoreFactory
-          .store('course_colors')
-          .delete(db)
-          .then((value) => log(name: 'app_database', 'Deleted old course color system'));
+      await intMapStoreFactory.store('course_colors').delete(db).then((value) =>
+          log(name: 'app_database', 'Deleted old course color system'));
     }
   }
 }
